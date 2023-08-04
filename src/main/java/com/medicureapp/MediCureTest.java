6e83ee5ceae7243b8f4562609daf6404c19f28a3
RAHUL KUMAR <rahulpatna89@gmail.com>
2023-08-05 00:35:18 +0530

    Update MediCureTest.java

--- src/main/java/com/medicureapp/MediCureTest.java
+++ src/main/java/com/medicureapp/MediCureTest.java
@@ -10,6 +10,48 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class MediCureTest {
 
+	public static void main(String[] args) throws InterruptedException, IOException {
+		System.setProperty("webdriver.chrome.driver","//home//ubuntu//chromedriver-linux64//chromedriver");
+		ChromeOptions chromeOptions = new ChromeOptions();
+		WebDriver driver = new ChromeDriver(chromeOptions);
+		driver.get("http://13.126.151.156:8082/contact.html");
+		
+		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+		
+		WebElement name= driver.findElement(By.xpath("//input[@placeholder='Your Name']"));
+		name.sendKeys("Rahul Kumar");
+		
+		
+		WebElement phone= driver.findElement(By.xpath("//input[@placeholder='Phone Number']"));
+		phone.sendKeys("9711739492");
+		
+		WebElement email= driver.findElement(By.xpath("//input[@placeholder='Email']"));
+		email.sendKeys("rahulpatna89@gmail.com");
+		
+		WebElement messagebox= driver.findElement(By.xpath("//input[@placeholder='Message']"));
+		messagebox.sendKeys("Hi Medicure , Please call me back for medical consultation");
+		
+		driver.findElement(By.className("btn_box")).click();
+		
+		String message = driver.findElement(By.id("message")).getText();
+		if(message.equals("Email sent.")) {
+			System.out.println("Script Executed Successfully");
+		} else 
+		{
+			System.out.println("Script Failed");
+		}
+		
+		TakesScreenshot scrShot = ((TakesScreenshot)driver);
+		File screenShot = scrShot.getScreenshotAs(OutputType.FILE);
+		File destFile = new File ("//home/ubuntu//scr.png");
+		FileUtils.copyFile(screenShot, destFile);
+		
+		Thread.sleep(3000);
+		
+		driver.quit();	
+
+
+	/*
 	//public static void main(String[] args) {
 	  //System.out.println("Test Case Executed Successfully");
 
@@ -31,6 +73,6 @@ public class MediCureTest {
 	}
 		
 			
-//}
+//} */
 
 }
