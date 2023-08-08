426545edfe6ac146f90912b05e810a38db2a64c8
RAHUL KUMAR <rahulpatna89@gmail.com>
2023-08-09 00:08:23 +0530

    Update MediCureTest.java

--- src/main/java/com/medicureapp/MediCureTest.java
+++ src/main/java/com/medicureapp/MediCureTest.java
@@ -10,18 +10,18 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class MediCureTest {
 
-		           @SuppressWarnings("deprecation")
-	           @Test
+		    @SuppressWarnings("deprecation")
+	            @Test
 	            public  void testapp() throws Exception {
-                WebDriverManager.chromedriver().setup();
+                    WebDriverManager.chromedriver().setup();
 		   WebDriver driver = new ChromeDriver();
 		   driver.manage().window().maximize();
 		   //System.setProperty("webdriver.chrome.driver","//home//ubuntu//chromedriver-linux64//chromedriver");
 
-		/*  ChromeOptions chromeOptions = new ChromeOptions();
-		chromeOptions.addArguments("--headless");
-		chromeOptions.addArguments("--no-sandbox");
-		chromeOptions.addArguments("--disable-dev-shm-usage"); */
+		  ChromeOptions chromeOptions = new ChromeOptions();
+		  chromeOptions.addArguments("--headless");
+		  chromeOptions.addArguments("--no-sandbox");
+		  chromeOptions.addArguments("--disable-dev-shm-usage");
 		  // driver.get("http://13.232.228.60:8082/contact.html");
 		   driver.get("http://3.110.56.142:8082/contact.html");
 		  
