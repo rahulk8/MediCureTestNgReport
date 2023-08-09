71b94dc7bb11d69a4109417569c9bd673a1ccd4a
RAHUL KUMAR <rahulpatna89@gmail.com>
2023-08-09 12:19:15 +0530

    Update MediCureTest.java

--- src/main/java/com/medicureapp/MediCureTest.java
+++ src/main/java/com/medicureapp/MediCureTest.java
@@ -10,72 +10,54 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class MediCureTest {
 
-		    @SuppressWarnings("deprecation")
-	            @Test
-	            public static void main(String[] args) throws InterruptedException, IOException {
-                    WebDriverManager.chromedriver().setup();
-		   WebDriver driver = new ChromeDriver();
-		   driver.manage().window().maximize();
-		   //System.setProperty("webdriver.chrome.driver","//home//ubuntu//chromedriver-linux64//chromedriver");
+		    //@SuppressWarnings("deprecation")
+      @Test
+      public static void main(String[] args) throws InterruptedException, IOException {
+       WebDriverManager.chromedriver().setup();
+       WebDriver driver = new ChromeDriver();
+       //driver.manage().window().maximize();
 
-		  ChromeOptions chromeOptions = new ChromeOptions();
-		  chromeOptions.addArguments("--headless");
-		  chromeOptions.addArguments("--no-sandbox");
-		  chromeOptions.addArguments("--disable-dev-shm-usage");
-		  // driver.get("http://13.232.228.60:8082/contact.html");
-		   driver.get("http://3.110.56.142:8082/contact.html");
-		  
-		   ////driver.findElement(By.name("q")).sendKeys("rahul",Keys.ENTER);
-		   //System.out.println(driver.getTitle());
+ ChromeOptions chromeOptions = new ChromeOptions();
+ chromeOptions.addArguments("--headless");
+ chromeOptions.addArguments("--no-sandbox");
+ chromeOptions.addArguments("--disable-dev-shm-usage"); 
+ 
+  driver.get("http://3.109.54.147:8082/contact.html");
 
-		  driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
-		
-		driver.findElement(By.xpath("//input[@placeholder='Your Name']")).sendKeys("Rahul Kumar");
-	
-		driver.findElement(By.xpath("//input[@placeholder='Phone Number']")).sendKeys("9711739492");
-	
-		
-		driver.findElement(By.xpath("//input[@placeholder='Email']")).sendKeys("rahulpatna89@gmail.com");
+   driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
 
-	        driver.findElement(By.xpath("//input[@placeholder='Message']")).sendKeys("Hi Medicure , Please call me back for medical consultation");
-		
-		driver.findElement(By.className("btn_box")).click();
-		
-		String message = driver.findElement(By.id("message")).getText();
-		if(message.equals("Email sent.")) {
-			//System.out.println("Script Executed Successfully");
-			TakesScreenshot scrShot = ((TakesScreenshot)driver);
-		        File screenShot = scrShot.getScreenshotAs(OutputType.FILE);
-		        File destFile = new File ("//home/ubuntu//scr.png");
-		        //File destFile = new File ("G:\\chrome-driver\\test-reports\\scr.png"); 
-		        //G:\\chrome-driver\\chromedriver.exe
-		       // File destFile = new File ("//var/lib/jenkins//scr.png");
-			//File destFile = new File ("//var//lib//jenkins//workspace//MediCureTestNgScreenshot//test-output//scr.png");
-			//File destFile = new File ("/var/lib/jenkins/workspace/health-care-pipeline/scr.png");
-			//var/lib/jenkins/workspace/MediCureTestNgScreenshot
-		         FileUtils.copyFile(screenShot, destFile); 
-			System.out.println("Screenshot taken Successfully");
-		} else 
-		{
-			//System.out.println("Script Failed");
-			System.out.println("Taking Screenshot gets failed");
-		}
-		
-	              /* TakesScreenshot scrShot = ((TakesScreenshot)driver);
-		        File screenShot = scrShot.getScreenshotAs(OutputType.FILE);
-		        File destFile = new File ("//home/ubuntu//scr.png")
-		        //File destFile = new File ("//var/lib/jenkins//scr.png");
-		        ///File destFile = new File ("./scr.png");
-		        FileUtils.copyFile(screenShot, destFile);  */
-		  
-		try {
-			Thread.sleep(5000);
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		driver.quit();	
+   driver.findElement(By.xpath("//input[@placeholder='Your Name']")).sendKeys("Rahul Kumar");
 
+   driver.findElement(By.xpath("//input[@placeholder='Phone Number']")).sendKeys("9711739492");
+
+
+   driver.findElement(By.xpath("//input[@placeholder='Email']")).sendKeys("rahulpatna89@gmail.com");
+
+   driver.findElement(By.xpath("//input[@placeholder='Message']")).sendKeys("Hi Medicure , Please call me back for medical consultation");
+
+   driver.findElement(By.className("btn_box")).click();
+
+String message = driver.findElement(By.id("message")).getText();
+if(message.equals("Email sent.")) {
+	TakesScreenshot scrShot = ((TakesScreenshot)driver);
+       File screenShot = scrShot.getScreenshotAs(OutputType.FILE);
+       File destFile = new File ("//home/ubuntu//scr.png");
+        FileUtils.copyFile(screenShot, destFile); 
+	System.out.println("Screenshot taken Successfully");
+} else 
+{
+	System.out.println("Taking Screenshot gets failed");
+}
+
+        
+ 
+try {
+	Thread.sleep(5000);
+} catch (InterruptedException e) {
+	// TODO Auto-generated catch block
+	e.printStackTrace();
+}
+driver.quit();
 
 	/*
 	
