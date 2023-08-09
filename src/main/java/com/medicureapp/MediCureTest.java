103e5ab4bee6210540766af73ec4bf75cbb6faf0
RAHUL KUMAR <rahulpatna89@gmail.com>
2023-08-09 12:19:55 +0530

    Update MediCureTest.java

--- src/main/java/com/medicureapp/MediCureTest.java
+++ src/main/java/com/medicureapp/MediCureTest.java
@@ -7,6 +7,22 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 import io.github.bonigarcia.wdm.WebDriverManager;
+import java.io.File;
+import org.testng.annotations.Test;
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+//import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class MediCureTest {
 
